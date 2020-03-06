36343ba58b84229428b6b16bdd307aa71f231627
Leandro Silva <le10sn@gmail.com>
2020-03-06 11:11:20 -0300

    added emit handlers

--- server/server.js
+++ server/server.js
@@ -1,31 +1,50 @@
 const path = require('path');
+const http = require('http');
 const express = require('express');
 const socketIO = require('socket.io');
-const http = require('http');
 
-
-const port = process.env.PORT || 3000;
 const publicPath = path.join(__dirname, '../public');
+const port = process.env.PORT || 3000;
+
 
 var app = express();
 var server = http.createServer(app);
 var io = socketIO(server);
 
-
-io.on('connection', (socket)=>{
-    console.log('New user connected');
-
-    socket.on('disconnect', ()=>{
-        console.log('Client disconnected');
-    });
-
-});
-
-
-
 app.use(express.static(publicPath));
 
-server.listen(port, ()=>{
-    console.log(`Server Running on port ${port}`);
+io.on('connection', (socket) => {
+  console.log('New user connected');
 
+  socket.emit('newMessage', {
+    from: 'Bozo',
+    text: 'Nois que voa ta ok?',
+    createdAt: new Date().getTime()
+  });
+
+  
+  socket.broadcast.emit('newMessage', {
+    from: 'Admin',
+    text: 'New user joined',
+    createdAt: new Date().getTime()
+  });
+
+  socket.on('createMessage', (message) => {
+    console.log('createMessage', message);
+    io.emit('newMessage', {
+      from: message.from,
+      text: message.text,
+      createdAt: new Date().getTime()
+    });
+
+   
+  });
+
+  socket.on('disconnect', function () {
+    console.log('Disconnected from server');
+  });
+});
+
+server.listen(port, () => {
+  console.log(`Server is running on ${port}`);
 });
